d9cfe898bd28606c9032984b57af30e7f7e01a9c
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-03-28 14:54:42 +0300

    submits dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+
+let mainRemover = document.querySelector("main")
+mainRemover.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.textContent = "STEVE is the champion."
+let body = document.querySelector("body")
+body.append(newHeader.id="victory")
